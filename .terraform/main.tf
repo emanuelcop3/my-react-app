813682f92a1b93d7112902b968740d9bfcd4bdbb
José Romualdo <joseromualdo@outlook.com>
2024-05-13 18:25:17 -0300

    create VM and deploy docker container

--- .terraform/main.tf
+++ .terraform/main.tf
@@ -3,33 +3,52 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_s3_bucket" "my_bucket" {
-  bucket = "josercf-state-bucket-tf"  # O nome deve ser globalmente único
-  acl    = "private"  # Define o bucket como privado
+resource "aws_security_group" "allow_web" {
+  name        = "allow_web_traffic"
+  description = "Allow web inbound traffic"
 
-  versioning {
-    enabled = true  # Opcional: habilita versionamento
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
+resource "aws_instance" "web" {
+  ami                    = "ami-057f57c2fcd14e5f4"
+  instance_type          = "t2.micro"
+  security_groups        = [aws_security_group.allow_web.name]
 
-resource "aws_s3_bucket_policy" "my_bucket_policy" {
-  bucket = aws_s3_bucket.my_bucket.id
+  user_data = <<-EOF
+                #!/bin/bash
+                sudo apt update
+                sudo apt install -y docker.io
+                sudo systemctl start docker
+                sudo systemctl enable docker
+              EOF
 
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action    = [
-          "s3:DeleteObject",
-          "s3:GetObject",
-          "s3:PutObject"
-        ]
-        Effect    = "Allow"
-        Resource  = "${aws_s3_bucket.my_bucket.arn}/*"
-        Principal = "*"  # Ajuste conforme a necessidade para especificar usuários ou roles
-      }
-    ]
-  })
+  tags = {
+    Name = "WebServer"
+  }
 }
 
+output "public_ip" {
+  value = aws_instance.web.public_ip
+}
